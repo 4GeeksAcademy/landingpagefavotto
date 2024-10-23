4e55acc4a77467a053916e6302bab92c4ca06a85
Aless <titifavotto@gmail.com>
2024-10-23 17:47:59 +0000

    terminado

--- src/js/component/jumbotron.jsx
+++ src/js/component/jumbotron.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 
 const Jumbotron = () => {
-    return (
-        <div className="jumbotron text-start px-5 ms-3 mt-5 mx-3">
-  <h1 className="display-4">A Warm Welcome!</h1>
-  <p className="lead">Lorem ipsum dolor sit amet consectetur adipiscing elit nulla libero purus pulvinar litora vitae, nam molestie lobortis aliquet luctus integer nec ac class rhoncus felis iaculis. Iaculis et blandit mi aenean posuere augue vulputate netus fusce taciti</p>
-  <a className="btn btn-primary btn-lg" href="#" role="button">Call to action!</a>
-</div>
-    )
-    
-}
+  return (
+    <div className="bg-light p-5 rounded-lg m-5 text-start">
+      <h1 className="display-4">A Warm Welcome!</h1>
+      <p className="lead">Lorem ipsum dolor sit amet consectetur adipiscing elit nulla libero purus pulvinar litora vitae, nam molestie lobortis aliquet luctus integer nec ac class rhoncus felis iaculis. Iaculis et blandit mi aenean posuere augue vulputate netus fusce tacitip</p>
+      <a className="btn btn-primary btn-lg" href="#" role="button">Call to action!</a>
+    </div>
+  );
+};
 
 export {Jumbotron}
